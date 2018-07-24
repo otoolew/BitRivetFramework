db5d5f8a496e48c04fea120970ea327f809cd935
O'Toole <WBO5@pitt.edu>
2018-07-24 14:22:18 -0400

    Fixed Animation issue

---
+++ Assets/Code/Core/Actor/NPCActor.cs
@@ -0,0 +1,29 @@
+﻿using RootMotion.FinalIK;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Core
+{
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(NavMeshAgent))]
+    [RequireComponent(typeof(AimIK))]
+    public class NPCActor : MonoBehaviour
+    {
+        Animator animator;
+        AimIK aimIK;
+        // Use this for initialization
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+            aimIK = GetComponent<AimIK>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Code/Core/Weapon/Range/WeaponController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Core
+{
+    public class WeaponController : MonoBehaviour
+    {
+        public RayCastLine rayLine;
+
+        public void FireRay()
+        {
+            rayLine.Fire();
+        }
+    }
+}
