0a9a0dce91bb7880d0211075f1244cb04f19935f
O'Toole <WBO5@pitt.edu>
2018-07-24 16:38:40 -0400

    Working on Health System.

---
+++ Assets/Code/Core/HealthSystem/HealthConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Core
+{
+    [CreateAssetMenu(fileName = "newHPConfig", menuName ="Health System/Health Configuration")]
+    public class HealthConfig : ScriptableObject
+    {
+        [Range(1, 100)]
+        public float MaxHealth;
+
+    }
+}
