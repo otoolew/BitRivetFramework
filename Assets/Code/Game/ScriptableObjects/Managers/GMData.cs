462cf8532dd30a3247d1b9044dc76e02cfc70c47
O'Toole <WBO5@pitt.edu>
2018-08-16 15:07:47 -0400

    Dialogue Interface work

---
+++ Assets/Code/Game/ScriptableObjects/Managers/GMData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newGameManagerData", menuName = "GameManager/GameData")]
+public class GMData : ScriptableObject
+{
+    public GameManager.GameState _currentGameState;
+    
+    public GameManager.GameState CurrentGameState
+    {
+        get { return _currentGameState; }
+        set { _currentGameState = value; }
+    }
+
+}
---
+++ Assets/Code/Game/ScriptableObjects/Player/PlayerData.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "newPlayerData", menuName = "GameManager/PlayerData")]
+public class PlayerData : ScriptableObject {
+    public bool IsHostile;
+}
